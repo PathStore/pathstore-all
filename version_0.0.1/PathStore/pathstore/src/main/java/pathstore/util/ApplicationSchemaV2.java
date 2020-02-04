e9039efdfe82dce1cdcfd396229169b69b0c473f
Myles Thiessen <myles.thiessen@mail.utoronto.ca>
2020-02-04 14:03:04 -0500

    Updated ApplicationSchemaV2 to take files from the command line, insert them into the database and write them to the pathstore_applications.apps table

--- version_0.0.1/PathStore/pathstore/src/main/java/pathstore/util/ApplicationSchemaV2.java
+++ version_0.0.1/PathStore/pathstore/src/main/java/pathstore/util/ApplicationSchemaV2.java
@@ -4,9 +4,16 @@ import com.datastax.driver.core.*;
 import com.datastax.driver.core.querybuilder.Insert;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Objective of this class is to write the current application schemas to a table in the root server's database
@@ -31,67 +38,15 @@ public class ApplicationSchemaV2 {
     /**
      * Constants class to represent all the sql strings / column names of tables that are modified throughout this class
      * TODO: Potential move to a global locations\
-     * TODO: Remove duplicate names
      * TODO: Write loader to load in cql files instead of hard coded tables
      */
     private static class Constants {
-        public static final String PATHSTORE_APPLICATIONS_KEYSPACE_CREATION = "CREATE KEYSPACE IF NOT EXISTS " + PathStoreApplications.KEYSPACE + " WITH replication = {'class' : 'SimpleStrategy', 'replication_factor' : 1 }  AND durable_writes = false;";
-        public static final String PATHSTORE_APPLICATIONS_APPS_TABLE =
-                "CREATE TABLE pathstore_applications.apps (" +
-                        "		appid int PRIMARY KEY," +
-                        "       key_space text," +
-                        //   "		code blob," +
-                        //   "		funcs list<int>," +
-                        //   "		owner text," +
-                        //   "		root_domain text," +
-                        "	    app_name text," +
-                        "	    schema_name text," +
-                        "	    app_schema text," +
-                        "time_created timestamp," +
-                        //   "	    app_schema_augmented text" +
-                        "	) WITH bloom_filter_fp_chance = 0.01" +
-                        "	    AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}" +
-                        "	    AND comment = 'table definitions'" +
-                        "	    AND compaction = {'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32', 'min_threshold': '4'}" +
-                        "	    AND compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}" +
-                        "	    AND crc_check_chance = 1.0" +
-                        "	    AND dclocal_read_repair_chance = 0.0" +
-                        "	    AND default_time_to_live = 0" +
-                        "	    AND gc_grace_seconds = 604800" +
-                        "	    AND max_index_interval = 2048" +
-                        "	    AND memtable_flush_period_in_ms = 3600000" +
-                        "	    AND min_index_interval = 128" +
-                        "	    AND read_repair_chance = 0.0" +
-                        "	    AND speculative_retry = '99PERCENTILE';";
-        public static final String PATHSTORE_DEMO_TABLE =
-                "CREATE TABLE pathstore_demo.users (" +
-                        "    name text PRIMARY KEY," +
-                        "    sport text," +
-                        "    years int," +
-                        "    vegetarian Boolean," +
-                        "    color list<int>" +
-                        ") WITH bloom_filter_fp_chance = 0.01" +
-                        "    AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}" +
-                        "    AND comment = 'table definitions'" +
-                        "    AND compaction = {'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32', 'min_threshold': '4'}" +
-                        "    AND compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}" +
-                        "    AND crc_check_chance = 1.0" +
-                        "    AND dclocal_read_repair_chance = 0.0" +
-                        "    AND default_time_to_live = 0" +
-                        "    AND gc_grace_seconds = 604800" +
-                        "    AND max_index_interval = 2048" +
-                        "    AND memtable_flush_period_in_ms = 3600000" +
-                        "    AND min_index_interval = 128" +
-                        "    AND read_repair_chance = 0.0" +
-                        "    AND speculative_retry = '99PERCENTILE';";
-
         public static final class PathStoreApplications {
             public static final String KEYSPACE = "pathstore_applications";
 
             public static final class Apps {
                 public static final String TABLE = "apps";
                 public static final String APPID = "appid";
-                public static final String KEYSPACE = "key_space";
                 public static final String APP_NAME = "app_name";
                 public static final String SCHEMA_NAME = "schema_name";
                 public static final String APP_SCHEMA = "app_schema";
@@ -120,73 +75,76 @@ public class ApplicationSchemaV2 {
 
         session = cluster.connect();
 
-        deleteSchema(Constants.PathStoreApplications.KEYSPACE);
-        loadPathStoreApplicationSchema();
-        insertApplicationSchema(
-                0,
-                Constants.PathStoreApplications.KEYSPACE,
-                Constants.PathStoreApplications.KEYSPACE,
-                Constants.PathStoreApplications.KEYSPACE,
-                Constants.PATHSTORE_APPLICATIONS_APPS_TABLE
-        );
-        deleteSchema("pathstore_demo");
-        createKeySpace("pathstore_demo");
-        insertApplicationSchema(1, "pathstore_demo", "demo", "demo-v1", Constants.PATHSTORE_DEMO_TABLE);
+        if (!new File(args[0]).getName().equals("pathstore_applications.cql")) {
+            System.out.println("You need to include the applications schema as the first parameter");
+            System.exit(1);
+        }
+
+        for (String s : getAllKeySpaces()) {
+            dropKeySpace(s);
+        }
+
+        try {
+            int appId = 0;
+            for (String s : args) {
+                File f = new File(s);
+                if (f.exists())
+                    insertApplicationSchema(appId++, f.getName(), f.getName(), readFileContents(s));
+                else
+                    System.out.println("File " + f.getName() + " does not exist");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         session.close();
         cluster.close();
 
     }
 
+    private static String readFileContents(final String fileName) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(fileName)));
+    }
+
+    private static List<String> getAllKeySpaces() {
+        ResultSet set = session.execute("select * from system_schema.keyspaces");
+
+        List<String> list = new LinkedList<>();
+
+        for (Row row : set) {
+            String keyspaceName = row.getString("keyspace_name");
+            if (keyspaceName.startsWith("pathstore_"))
+                list.add(keyspaceName);
+        }
+
+        return list;
+    }
+
     /**
      * TODO: Move string literals
-     *
-     * @param schemaName
      */
-    private static void deleteSchema(final String schemaName) {
-        String dropSchemaStatement = "drop keyspace if exists " + schemaName;
-        System.out.println("Dropping keyspace " + schemaName);
+    private static void dropKeySpace(final String keySpace) {
+        String dropSchemaStatement = "drop keyspace if exists " + keySpace;
+        System.out.println("Dropping keyspace " + keySpace);
         session.execute(dropSchemaStatement);
         System.out.println("Keyspace dropped");
-    }
-
-    /**
-     * TODO: Export table to file instead of hard coded feature.
-     * <p>
-     * Creates pathstore_applications keyspace
-     * Inserts One table this table is apps it has the following properties
-     * <p>
-     * appid: application id bounds must be greater then or equal to 1
-     * app_name: name of application
-     * keyspace: keyspace name
-     * schema_name: i.e for version control
-     * app_schema: literal schema stored in plain text
-     * time_created: time of creation for log of schemas
-     */
-    private static void loadPathStoreApplicationSchema() {
-        System.out.println("Creating keyspace");
-        session.execute(Constants.PATHSTORE_APPLICATIONS_KEYSPACE_CREATION);
-        System.out.println("Keyspace created");
-    }
-
-    /**
-     * TODO: Move string literal
-     */
-    private static void createKeySpace(final String keySpaceName) {
-        session.execute("CREATE KEYSPACE IF NOT EXISTS " + keySpaceName + " WITH replication = {'class' : 'SimpleStrategy', 'replication_factor' : 1 }  AND durable_writes = false;");
     }
 
     /**
      * TODO: Make it so that it does not override current app schema of same name.
      *
      * @param appId      application Id. Must be greater then 1 as 0 is reserved for the pathstore_application schema
-     * @param keyspace   keyspace the schema is apart of
      * @param appName    name of application. Easier identifier then its associated appId
      * @param schemaName name of schema. This is used for version control. I.e $appName-0.0.1
      * @param schema     literal contents of schema. This is used to distribute the schema to lower nodes
      */
-    private static void insertApplicationSchema(final int appId, final String keyspace, final String appName, final String schemaName, final String schema) {
-        session.execute(schema);
+    private static void insertApplicationSchema(final int appId, final String appName, final String schemaName, final String schema) {
+        String[] commands = schema.split(";");
+        for (String c : commands) {
+            String cql = c.trim();
+            if (cql.length() > 0)
+                session.execute(cql);
+        }
 
         System.out.println("Inserting schema");
         Insert insert = QueryBuilder.insertInto(
@@ -197,10 +155,6 @@ public class ApplicationSchemaV2 {
         insert.value(
                 Constants.PathStoreApplications.Apps.APPID,
                 appId
-        );
-        insert.value(
-                Constants.PathStoreApplications.Apps.KEYSPACE,
-                keyspace
         );
         insert.value(
                 Constants.PathStoreApplications.Apps.APP_NAME
