edb8e58cb7dc6a0460a059aa551d478891849050
Myles Thiessen <myles.thiessen@mail.utoronto.ca>
2020-02-29 18:51:00 -0700

    Added basic deletion functionality algorithm

--- version_0.0.1/PathStore/pathstore/src/main/java/pathstore/util/ApplicationInstaller.java
+++ version_0.0.1/PathStore/pathstore/src/main/java/pathstore/util/ApplicationInstaller.java
@@ -66,7 +66,53 @@ public class ApplicationInstaller {
     session.execute(batchStatement);
   }
 
-  private static void remove_application(final int nodeid, final String keyspace_name) {}
+  private static void remove_application(
+      final Session session, final int nodeid, final String keyspace_name) {
+    System.out.println("Removing for Node: " + nodeid + " with application " + keyspace_name);
+
+    // TODO: Check if keyspace_name is a valid keyspace
+
+    Map<Integer, Integer> parent_to_node = new HashMap<>();
+
+    // Creates map from current nodeid to parent's node id
+    for (Row row :
+        session.execute(QueryBuilder.select().all().from("pathstore_applications", "topology")))
+      parent_to_node.put(row.getInt("parent_nodeid"), row.getInt("nodeid"));
+
+    System.out.println(parent_to_node);
+
+    List<ApplicationEntry> applicationEntryList = new LinkedList<>();
+
+    Integer current_nodeid = nodeid;
+
+    while (current_nodeid != null) {
+      Integer child_nodeid = parent_to_node.get(current_nodeid);
+      ApplicationEntry new_entry =
+          new ApplicationEntry(
+              current_nodeid,
+              ProccessStatus.WAITING_REMOVE,
+              child_nodeid == null ? -1 : child_nodeid);
+      applicationEntryList.add(new_entry);
+      System.out.println(new_entry);
+      current_nodeid = child_nodeid;
+    }
+
+    /*
+    BatchStatement batchStatement = new BatchStatement();
+
+    for (ApplicationEntry entry : applicationEntryList)
+      batchStatement.add(
+          QueryBuilder.insertInto("pathstore_applications", "node_schemas")
+              .value("pathstore_version", QueryBuilder.now())
+              .value("pathstore_parent_timestamp", QueryBuilder.now())
+              .value("keyspace_name", keyspace_name)
+              .value("nodeid", entry.node_id)
+              .value("process_status", entry.proccess_status.toString())
+              .value("wait_for", entry.waiting_for));
+
+    session.execute(batchStatement);
+     */
+  }
 
   public static void main(String[] args) {
     switch (args[0]) {
@@ -91,6 +137,24 @@ public class ApplicationInstaller {
         cluster.close();
         break;
       case "remove":
+        cluster =
+            new Cluster.Builder()
+                .addContactPoints(PathStoreProperties.getInstance().CassandraIP)
+                .withPort(PathStoreProperties.getInstance().CassandraPort)
+                .withSocketOptions(
+                    new SocketOptions().setTcpNoDelay(true).setReadTimeoutMillis(15000000))
+                .withQueryOptions(
+                    new QueryOptions()
+                        .setRefreshNodeIntervalMillis(0)
+                        .setRefreshNodeListIntervalMillis(0)
+                        .setRefreshSchemaIntervalMillis(0))
+                .build();
+        session = cluster.connect();
+
+        remove_application(session, Integer.parseInt(args[1]), args[2]);
+
+        session.close();
+        cluster.close();
         break;
       default:
         System.err.println(args[0] + " is not an option");
