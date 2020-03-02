915b7f7dc0374934262518d5e72b25bbb769329b
Myles Thiessen <myles.thiessen@mail.utoronto.ca>
2020-03-01 18:59:59 -0700

    Added basic react spring install for pathstore admin panel

---
+++ version_0.0.1/PathStore/pathstore-admin-panel/frontend/src/App.js
@@ -0,0 +1,28 @@
+import React, {Component, useState, useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        fetch('/api/hello')
+            .then(response => response.text())
+            .then(message => {
+                setMessage(message);
+            });
+    }, []);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1 className="App-title">{message}</h1>
+            </header>
+            <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+        </div>
+    )
+}
+
+export default App;
---
+++ version_0.0.1/PathStore/pathstore-admin-panel/src/main/java/pathstore/pathstoreadminpanel/PathstoreAdminPanelApplication.java
@@ -0,0 +1,13 @@
+package pathstore.pathstoreadminpanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathstoreAdminPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathstoreAdminPanelApplication.class, args);
+	}
+
+}
