a19d244ac0484305c0c19436d714d5e039bf5160
Myles Thiessen <myles.thiessen@mail.utoronto.ca>
2020-04-14 22:17:00 -0400

    Added comments to View Topology

--- version_0.0.1/PathStore/pathstore-admin-panel/frontend/src/modules/ViewTopology.js
+++ version_0.0.1/PathStore/pathstore-admin-panel/frontend/src/modules/ViewTopology.js
@@ -2,7 +2,19 @@ import React, {Component} from "react";
 import Tree from "react-tree-graph";
 import Info from "./Info";
 
+/**
+ * This class is used to display a visual of the network topology of the network using the Tree package.
+ */
 export default class ViewTopology extends Component {
+
+    /**
+     * @param props sends refresh from parent module, and topology queried from parent module
+     *
+     * State:
+     *  - topology: from props
+     *  - info: module to load if the user clicks on a node
+     *  - refreshInfo: to allow for creation of multiple info modals without refreshing viewtopology
+     */
     constructor(props) {
         super(props);
         this.state = {
@@ -12,29 +24,43 @@ export default class ViewTopology extends Component {
         };
     }
 
+    /**
+     * Called when parent props change. If refresh has changed from the local copy then re-create the tree
+     */
     componentWillReceiveProps(props, nextContext) {
         if (this.props.refresh !== props.refresh) {
             this.setState({topology: this.createTree(props.topology, -1)})
         }
     }
 
+    /**
+     * Creates interpretable json object for the Tree package
+     *
+     * @param array from parent
+     * @param parentId -1 for initial call to look for the root node and work way down
+     * @returns {{textProps: {x: number, y: number}, children: [], name: *}|[]}
+     */
     createTree = (array, parentId) => {
         let children = [];
 
         for (let i = 0; i < array.length; i++)
             if (parentId === -1) {
-                if (array[i].parentid === parentId) {
-                    return this.createTreeObject(array[i].id, array);
-                }
+                if (array[i].parentid === parentId) return this.createTreeObject(array[i].id, array);
             } else {
-                if (array[i].parentid === parentId)
-                    children.push(this.createTreeObject(array[i].id, array));
+                if (array[i].parentid === parentId) children.push(this.createTreeObject(array[i].id, array));
             }
 
 
         return children;
     };
 
+    /**
+     * Name is the node id, textProps is the location of the text associated with the node, children is a list of children
+     *
+     * @param name
+     * @param array
+     * @returns {{textProps: {x: number, y: number}, children: ({textProps: {x: number, y: number}, children: *[], name: *}|*[]), name: *}}
+     */
     createTreeObject = (name, array) => {
         return {
             name: name,
@@ -43,6 +69,12 @@ export default class ViewTopology extends Component {
         }
     };
 
+    /**
+     * Function that is called when a node is clicked.
+     *
+     * @param event not used.
+     * @param node node id
+     */
     handleClick = (event, node) => {
         this.setState({
             info: <Info node={node} refresh={!this.state.refreshInfo}/>,
@@ -50,6 +82,11 @@ export default class ViewTopology extends Component {
         })
     };
 
+    /**
+     * Renders a header and the tree. Also optional an info modal
+     *
+     * @returns {*}
+     */
     render() {
         return (
             <div>
