e0d53706017decca81346336b8d49df939fea972
Myles Thiessen <myles.thiessen@mail.utoronto.ca>
2020-04-16 15:11:15 -0400

    Added comments to loading model and updated todos

--- version_0.0.1/PathStore/pathstore-admin-panel/frontend/src/modules/LoadingModel.js
+++ version_0.0.1/PathStore/pathstore-admin-panel/frontend/src/modules/LoadingModel.js
@@ -3,24 +3,36 @@ import Modal from "react-modal";
 
 /**
  * TODO: Show some spinning image or something
+ *
+ * This is a simple loading model to be shown when the user has made a request that may take some time.
+ *
+ * An example of this is when the user creates an application as the general time this takes is 9 secs.
  */
 export default class LoadingModel extends Component {
+
+    /**
+     * @param props contains show which denotes whether to show or note
+     */
     constructor(props) {
         super(props);
-        this.state = {
-            show: props.show
-        }
     }
 
+    /**
+     * Update current props show with new show value if they're not equal
+     * @param props
+     * @param nextContext
+     */
     componentWillReceiveProps(props, nextContext) {
-        if (this.props.show !== props.show) {
-            this.setState({show: props.show});
-        }
+        if (this.props.show !== props.show) this.props.show = this.props.show;
     }
 
+    /**
+     * Show a modal with a loading image TODO
+     * @returns {*}
+     */
     render() {
         return (
-            <Modal isOpen={this.state.show}>
+            <Modal isOpen={this.props.show}>
                 <p>Loading</p>
             </Modal>
         );
