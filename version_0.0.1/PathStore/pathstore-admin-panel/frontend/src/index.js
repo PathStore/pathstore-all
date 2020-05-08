cb927441b59801c3673c787cf8af987860f3ffde
Myles Thiessen <myles.thiessen@mail.utoronto.ca>
2020-05-08 01:52:12 -0600

    Removed js code base and moved typescript code base and have successfully test maven packge functionality for the api

--- version_0.0.1/PathStore/pathstore-admin-panel/frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './Circles.css';
-import PathStoreControlPanel from './PathStoreControlPanel'
-
-ReactDOM.render(<PathStoreControlPanel/>, document.getElementById('root'));
