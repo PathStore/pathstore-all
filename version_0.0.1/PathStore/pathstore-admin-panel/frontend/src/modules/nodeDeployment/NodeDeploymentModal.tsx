bca0f55a65c272d179ea207abdb909a7762a82dc
Myles Thiessen <myles.thiessen@mail.utoronto.ca>
2020-05-19 16:22:49 -0600

    Fixed bug where on deletion of a hypothetical node it would not delete it's children (if any)

--- version_0.0.1/PathStore/pathstore-admin-panel/frontend/src/modules/nodeDeployment/NodeDeploymentModal.tsx
+++ version_0.0.1/PathStore/pathstore-admin-panel/frontend/src/modules/nodeDeployment/NodeDeploymentModal.tsx
@@ -184,11 +184,93 @@ export default class NodeDeploymentModal extends Component<NodeDeploymentModalPr
         this.setState({
             infoModalShow: false,
             infoModalIsHypothetical: false
-        }, () => this.setState({
-            deployment: this.state.deployment.filter(i => i.new_node_id !== this.state.infoModalNodeNumber),
-            updates: this.state.updates.filter(i => i.newNodeId !== this.state.infoModalNodeNumber),
-            infoModalNodeNumber: -1
-        }));
+        }, () =>
+            this.setState(this.deleteNodeAndChildren(this.state.deployment, this.state.updates, this.state.infoModalNodeNumber)));
+    };
+
+    /**
+     * This function is used to handle the deletion of a hypothetical node from the hypothetical network builder.
+     *
+     * What this algorithm does is that it takes the list of deployment and parses it into a map, This map is a representation
+     * from P -> List<Deployment> where P is the parent node id. This is used to find a list of children based on some node number.
+     * If the response is undefined you can determine that, that node id has no children.
+     *
+     * If the node that has been requested to be deleted has no children we can simply filter out that node id from
+     * the deployment list and the updates list.
+     *
+     * If the node has children then you need to recursively check to see if children exist and delete them from
+     * both lists as well. In this case it is easier to do the work inside the map and then after produce a list
+     * based on the map, this doesn't produce any additional problems because the ordering of deployment does not
+     * matter.
+     *
+     * @param deployment list of deployment objects
+     * @param updates list of update objects
+     * @param node_number node id requested to be deleted
+     * @return updated state
+     */
+    deleteNodeAndChildren = (deployment: Deployment[], updates: Update[], node_number: number):
+        { deployment: Deployment[], updates: Update[], infoModalNodeNumber: number } => {
+
+        const deploymentMap: Map<number, Deployment[]> = new Map<number, Deployment[]>();
+
+        for (let i = 0; i < deployment.length; i++)
+            if (deployment[i].new_node_id !== node_number) {
+                if (!deploymentMap.has(deployment[i].parent_node_id)) deploymentMap.set(deployment[i].parent_node_id, [deployment[i]]);
+                else deploymentMap.get(deployment[i].parent_node_id)?.push(deployment[i]);
+            }
+
+        if (!deploymentMap.has(node_number))
+            return {
+                deployment: deployment.filter(i => i.new_node_id !== node_number),
+                updates: updates.filter(i => i.newNodeId !== node_number),
+                infoModalNodeNumber: -1
+            };
+        else {
+
+            const updatesMap: Map<number, Update> = new Map<number, Update>();
+
+            for (let i = 0; i < updates.length; i++)
+                updatesMap.set(updates[i].newNodeId, updates[i]);
+
+            this.deleteNodeAndChildrenHelper(deploymentMap, updatesMap, node_number);
+
+            let newDeployment: Deployment[] = [];
+
+            deploymentMap.forEach((value: Deployment[]) => newDeployment = newDeployment.concat(value));
+
+            const newUpdates: Update[] = [];
+
+            updatesMap.forEach((value: Update) => newUpdates.push(value));
+
+            return {
+                deployment: newDeployment,
+                updates: newUpdates,
+                infoModalNodeNumber: -1
+            };
+        }
+    };
+
+    /**
+     * Helper function to recursively delete all children based on a given node id.
+     *
+     * @param deploymentMap deployment map to remove from
+     * @param updatesMap updates map to remove from
+     * @param node which node to delete
+     */
+    deleteNodeAndChildrenHelper = (deploymentMap: Map<number, Deployment[]>, updatesMap: Map<number, Update>, node: number): void => {
+        if (deploymentMap.has(node)) {
+            const children: Deployment[] | undefined = deploymentMap.get(node);
+
+            if (children !== undefined) {
+                for (let i = 0; i < children.length; i++) {
+                    this.deleteNodeAndChildrenHelper(deploymentMap, updatesMap, children[i].new_node_id);
+                    updatesMap.delete(children[i].new_node_id);
+                }
+
+                updatesMap.delete(node);
+                deploymentMap.delete(node);
+            }
+        }
     };
 
     /**
